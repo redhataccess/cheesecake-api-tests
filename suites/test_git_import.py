c7186f580da372dc696bd6face28ca2d5d1f4c65
pournima badak <pbadak@localhost.localdomain>
2021-04-28 19:46:21 +0530

    adding tests for git2pantheon API's

--- suites/test_git_import.py
+++ suites/test_git_import.py
@@ -13,19 +13,21 @@ from polling2 import poll
 git_import_repo_URL = base.config_reader('git_import_test_repo', 'git_import_repo_url')
 git_import_repo_Name = base.config_reader('git_import_test_repo', 'git_import_repo_name')
 git_import_repo_branch = base.config_reader('git_import_test_repo', 'git_import_repo_branch')
-number_of_modules = base.config_reader('git_import_test_repo', 'number_of_modules_imported')
-module_title_prefix = base.config_reader('git_import_test_repo', 'module_prefix')
-assembly_title_prefix = base.config_reader('git_import_test_repo', 'assembly_prefix')
+# number_of_modules = base.config_reader('git_import_test_repo', 'number_of_modules_imported')
+# module_title_prefix = base.config_reader('git_import_test_repo', 'module_prefix')
+# assembly_title_prefix = base.config_reader('git_import_test_repo', 'assembly_prefix')
 number_of_resources_uploaded = base.config_reader('git_import_test_repo', 'number_of_resources_uploaded')
+number_of_modules_uploaded = base.config_reader('git_import_test_repo', 'number_of_modules_uploaded')
+number_of_assemblies_uploaded = base.config_reader('git_import_test_repo', 'number_of_assemblies_uploaded')
+
 
 @lcc.suite(description="Suite: Git import functionality", rank=6)
 class test_git_import:
     api_auth = lcc.inject_fixture("api_auth")
+    status_key = ""
 
-    # @lcc.disabled()
-    @lcc.test("Verify that git import API is able to git import modules successfully")
-    def git_import_api(self):
-        lcc.log_info("Checking for post request to git import functionality...")
+    @lcc.test("Verify that git clone API is able to upload files successfully and get status_key from response")
+    def git_clone_api(self):
         payload = {
             "branch": git_import_repo_branch,
             "repo": git_import_repo_URL
@@ -34,87 +36,81 @@ class test_git_import:
         payload = json.dumps(payload)
         lcc.log_info(str(payload))
         git_import_req = requests.post(fixture.git_import_server + "/api/clone", data=payload)
-        lcc.log_info(git_import_req.url)
-        time.sleep(15)
-        print("Response code for git import request:" + str(git_import_req.status_code))
-        print(git_import_req.content)
+        check_that("POST request to git clone was done", git_import_req.status_code, equal_to(202))
+        self.status_key = git_import_req.content
+        time.sleep(5)
+        git_status_response = requests.post(fixture.git_import_server + "/api/status", data=self.status_key)
+        check_that("git import status", git_status_response.json()["status"], contains_string("uploading"))
 
-        # print("Response json: " + str(git_import_req.json()))
-        require_that("POST request to git import was done", git_import_req.status_code, equal_to(202))
-        git_clone_response = git_import_req.content
+    @lcc.test("Verify that resources are uploaded")
+    def check_resources_api(self):
+        poll(lambda: requests.post(fixture.git_import_server + "/api/progress-update/resources", data=self.status_key),
+             check_success=check_current_status, step=5, timeout=60)
+        resources_response = requests.post(fixture.git_import_server + "/api/progress-update/resources",
+                                           data=self.status_key)
+        check_that("resources API status code", resources_response.status_code, equal_to(200))
+        check_that("current status", resources_response.json()["current_status"], equal_to("done"))
+        check_that("number of resources uploaded", resources_response.json()["total_files_uploaded"],
+                   equal_to(int(number_of_resources_uploaded)))
+        check_that("resources not uploaded", resources_response.json()["resources_not_uploaded"], equal_to([]))
+        check_that("server message", resources_response.json()["server_message"], equal_to("Accepting requests"))
+        check_that("server status", resources_response.json()["server_status"], equal_to("OK"))
+        check_that("last_uploaded_file", resources_response.json()["last_uploaded_file"], is_not_none())
+        check_that("modules not uploaded", resources_response.json()["resources_not_uploaded"], equal_to([]))
+        resources_uploaded = resources_response.json()["resources_uploaded"]
+        check_response_code(resources_uploaded, "response_code", int(number_of_resources_uploaded))
+        check_not_null(resources_uploaded, "path", int(number_of_resources_uploaded))
+        check_not_null(resources_uploaded, "response_details", int(number_of_resources_uploaded))
 
-        # search_url = fixture.url + 'pantheon/internal/modules.json?search=' + module_title_prefix
-        # lcc.log_info("Git import functionality verified using endpoint: %s" % search_url)
-        # lcc.log_info("Trying to poll the endpoint until we get the required number of search results as"
-        #              " per the test data ...")
-        #
-        # imported_modules_array = []
-        #
-        # def check_if_imported(response):
-        #     time.sleep(2)
-        #     print("Response code for 'search' api request: " + str(response.status_code))
-        #     result1 = response.json()["results"]
-        #     lcc.log_info(
-        #         "list of results found in total but not specific to this request: %s" % str(response.json()["size"]))
-        #     print("Number of results found which were uploaded for git import: " + str(response.json()["size"]))
-        #     for result in result1:
-        #         if result["pant:transientSourceName"] == git_import_repo_Name:
-        #             imported_modules_array.append(result["jcr:title"])
-        #             print("Found a matching result, waiting for more...")
-        #
-        #         else:
-        #             print("No matching result found yet...")
-        #             time.sleep(2)
-        #     check_that("Count of modules uploaded using git import", len(imported_modules_array),
-        #                greater_than_or_equal_to(int(number_of_modules)))
-        #     return len(imported_modules_array) >= int(number_of_modules)
+    @lcc.test("Verify that modules are uploaded")
+    def check_modules_api(self):
+        poll(lambda: requests.post(fixture.git_import_server + "/api/progress-update/modules", data=self.status_key),
+             check_success=check_current_status, step=5, timeout=20)
+        modules_response = requests.post(fixture.git_import_server + "/api/progress-update/modules",
+                                         data=self.status_key)
+        check_that("modules API status code", modules_response.status_code, equal_to(200))
+        check_that("current status", modules_response.json()["current_status"], equal_to("done"))
+        check_that("number of modules uploaded", modules_response.json()["total_files_uploaded"],
+                   equal_to(int(number_of_modules_uploaded)))
+        check_that("modules not uploaded", modules_response.json()["modules_not_uploaded"], equal_to([]))
+        check_that("server message", modules_response.json()["server_message"], equal_to("Accepting requests"))
+        check_that("server status", modules_response.json()["server_status"], equal_to("OK"))
+        check_that("last_uploaded_file", modules_response.json()["last_uploaded_file"], is_not_none())
+        modules_uploaded = modules_response.json()["modules_uploaded"]
+        check_response_code(modules_uploaded, "response_code", int(number_of_modules_uploaded))
+        check_not_null(modules_uploaded, "path", int(number_of_modules_uploaded))
+        check_not_null(modules_uploaded, "response_details", int(number_of_modules_uploaded))
 
-        # poll(lambda: requests.get(search_url), check_success=check_if_imported, step=5, timeout=60)
+    @lcc.test("Verify that assemblies are uploaded")
+    def check_assemblies_api(self):
+        poll(lambda: requests.post(fixture.git_import_server + "/api/progress-update/assemblies", data=self.status_key),
+             check_success=check_current_status, step=5, timeout=20)
+        assemblies_response = requests.post(fixture.git_import_server + "/api/progress-update/assemblies",
+                                            data=self.status_key)
+        check_that("modules API status code", assemblies_response.status_code, equal_to(200))
+        check_that("current status", assemblies_response.json()["current_status"], equal_to("done"))
+        check_that("number of assemblies uploaded", assemblies_response.json()["total_files_uploaded"],
+                   equal_to(int(number_of_assemblies_uploaded)))
+        check_that("assemblies not uploaded", assemblies_response.json()["assemblies_not_uploaded"], equal_to([]))
+        check_that("server message", assemblies_response.json()["server_message"], equal_to("Accepting requests"))
+        check_that("server status", assemblies_response.json()["server_status"], equal_to("OK"))
+        check_that("last_uploaded_file", assemblies_response.json()["last_uploaded_file"], is_not_none())
+        assemblies_uploaded = assemblies_response.json()["assemblies_uploaded"]
+        check_response_code(assemblies_uploaded, "response_code", int(number_of_assemblies_uploaded))
+        check_not_null(assemblies_uploaded, "path", int(number_of_assemblies_uploaded))
+        check_not_null(assemblies_uploaded, "response_details", int(number_of_assemblies_uploaded))
 
-        git_status_res = requests.post(fixture.git_import_server + "/api/status", data=git_clone_response)
-        require_that("POST request to git resources was done", git_status_res.status_code, equal_to(200))
-        print(str(git_status_res.content))
+    @lcc.test("Verify progress-update/all API works as expected")
+    def check_progress_update_all_api(self):
+        response = requests.post(fixture.git_import_server + "/api/progress-update/all",
+                                 data=self.status_key)
+        check_that("progress-update/all API status code", response.status_code, equal_to(200))
+        check_that("current status", response.json()["current_status"], equal_to("done"))
+        check_that("status code for module variant created",
+                   response.json()["module_variants_created"][0]["response_code"], equal_to(201))
 
-        def check_current_status(response):
-            json_response = response.json()
-            current_status = json_response["current_status"]
-            lcc.log_info(current_status)
-            return current_status == "done"
-
-        poll(lambda: requests.post(fixture.git_import_server + "/api/progress-update/resources", data=git_clone_response),
-                        check_success=check_current_status, step=2, timeout=10)
-
-        poll(lambda: requests.post(fixture.git_import_server + "/api/progress-update/modules",data=git_clone_response,
-             check_success=check_current_status, step=2, timeout=10))
-
-        poll(lambda: requests.post(fixture.git_import_server + "/api/progress-update/assemblies",data=git_clone_response,
-             check_success=check_current_status, step=2, timeout=10))
-
-
-
-
-        # require_that("POST request to progress update resources was done", progress_update_resources_res.status_code,
-        #              equal_to(200))
-        #
-        # progress_update_res = requests.post(fixture.git_import_server + "/api/progress-update/all",
-        #                                     data=git_clone_response)
-        # require_that("POST request to progress update was done", progress_update_res.status_code, equal_to(200))
-        # lcc.log_info("progress update response:" + str(progress_update_res.content))
-        #
-        # progress_update_assemblies_res = requests.post(fixture.git_import_server + "/api/progress-update/assemblies",
-        #                                                data=git_clone_response)
-        # require_that("POST request to progress update assemblies was done", progress_update_assemblies_res.status_code,
-        #              equal_to(200))
-        # lcc.log_info("progress update assembly response:" + str(progress_update_assemblies_res.content))
-        #
-        # progress_update_modules_res = requests.post(fixture.git_import_server + "/api/progress-update/modules",
-        #                                             data=git_clone_response)
-        # require_that("POST request to progress update modules was done", progress_update_modules_res.status_code,
-        #              equal_to(200))
-        # lcc.log_info("progress update modules response:" + str(progress_update_modules_res.content))
-        #
-        #
-        # lcc.log_info("progress update resources response:" + str(progress_update_resources_res.content))
+        git_status_response = requests.post(fixture.git_import_server + "/api/status", data=self.status_key)
+        check_that("git import status", git_status_response.json()["status"], contains_string("done"))
 
     def teardown_suite(self):
         # Deleting the git repo uploaded via git import in the test suite.
@@ -122,6 +118,28 @@ class test_git_import:
         path_to_git_repo = fixture.url + "bin/cpm/nodes/node.json/content/repositories/" + git_import_repo_Name
         lcc.log_info("Test repo node used for git import functionality being deleted at: %s" % path_to_git_repo)
         response_git_delete = requests.delete(path_to_git_repo, auth=(fixture.admin_username, fixture.admin_auth))
-        print(str(response_git_delete.content))
+        # print(str(response_git_delete.content))
         check_that("The git import test repo was deleted successfully from backend", response_git_delete.status_code,
                    equal_to(200))
+
+
+def check_current_status(response):
+    current_status = response.json()["current_status"]
+    print(current_status)
+    return current_status == "done"
+
+
+def check_response_code(response, field_name, expected_count):
+    count = 0
+    for res in response:
+        if res[field_name] == 201 or res[field_name] == 200:
+            count += 1
+    check_that("count of " + field_name + " : 201 should be ", count, equal_to(expected_count))
+
+
+def check_not_null(response, field_name, expected_count):
+    count = 0
+    for res in response:
+        if res[field_name] is not None:
+            count += 1
+    check_that("count of " + field_name + " should not be null ", count, equal_to(expected_count))
